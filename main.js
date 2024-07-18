a6e31d36887c661b071c7d6e23cb241171d910f4
RachitRawat09 <rawatritik5670@gmail.com>
2024-07-18 15:05:39 +0530

    commit

---
+++ main.js
@@ -0,0 +1,47 @@
+// JavaScript for slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+const menuIcon = document.querySelector(".menu-icon");
+const content = document.querySelector(".content");
+
+menuIcon.addEventListener("click", () => {
+  content.style.display = content.style.display === "block" ? "none" : "block";
+});
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
